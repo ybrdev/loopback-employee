0365b1f47c04d2df887d4e8529de8b06c6a5802d
Yusuf Badurohman <yusuf.badurohman@gmail.com>
2018-11-14 15:43:57 +0700

    feat(controllers): add employee controller

---
+++ src/controllers/employee.controller.ts
@@ -0,0 +1,122 @@
+import {
+  Count,
+  CountSchema,
+  Filter,
+  repository,
+  Where,
+} from '@loopback/repository';
+import {
+  post,
+  param,
+  get,
+  getFilterSchemaFor,
+  getWhereSchemaFor,
+  patch,
+  del,
+  requestBody,
+} from '@loopback/rest';
+import {Employee} from '../models';
+import {EmployeeRepository} from '../repositories';
+
+export class EmployeeController {
+  constructor(
+    @repository(EmployeeRepository)
+    public employeeRepository : EmployeeRepository,
+  ) {}
+
+  @post('/employees', {
+    responses: {
+      '200': {
+        description: 'Employee model instance',
+        content: {'application/json': {schema: {'x-ts-type': Employee}}},
+      },
+    },
+  })
+  async create(@requestBody() employee: Employee): Promise<Employee> {
+    return await this.employeeRepository.create(employee);
+  }
+
+  @get('/employees/count', {
+    responses: {
+      '200': {
+        description: 'Employee model count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  async count(
+    @param.query.object('where', getWhereSchemaFor(Employee)) where?: Where,
+  ): Promise<Count> {
+    return await this.employeeRepository.count(where);
+  }
+
+  @get('/employees', {
+    responses: {
+      '200': {
+        description: 'Array of Employee model instances',
+        content: {
+          'application/json': {
+            schema: {type: 'array', items: {'x-ts-type': Employee}},
+          },
+        },
+      },
+    },
+  })
+  async find(
+    @param.query.object('filter', getFilterSchemaFor(Employee)) filter?: Filter,
+  ): Promise<Employee[]> {
+    return await this.employeeRepository.find(filter);
+  }
+
+  @patch('/employees', {
+    responses: {
+      '200': {
+        description: 'Employee PATCH success count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  async updateAll(
+    @requestBody() employee: Employee,
+    @param.query.object('where', getWhereSchemaFor(Employee)) where?: Where,
+  ): Promise<Count> {
+    return await this.employeeRepository.updateAll(employee, where);
+  }
+
+  @get('/employees/{id}', {
+    responses: {
+      '200': {
+        description: 'Employee model instance',
+        content: {'application/json': {schema: {'x-ts-type': Employee}}},
+      },
+    },
+  })
+  async findById(@param.path.number('id') id: number): Promise<Employee> {
+    return await this.employeeRepository.findById(id);
+  }
+
+  @patch('/employees/{id}', {
+    responses: {
+      '204': {
+        description: 'Employee PATCH success',
+      },
+    },
+  })
+  async updateById(
+    @param.path.number('id') id: number,
+    @requestBody() employee: Employee,
+  ): Promise<void> {
+    await this.employeeRepository.updateById(id, employee);
+  }
+
+  @del('/employees/{id}', {
+    responses: {
+      '204': {
+        description: 'Employee DELETE success',
+      },
+    },
+  })
+  async deleteById(@param.path.number('id') id: number): Promise<void> {
+    await this.employeeRepository.deleteById(id);
+  }
+}
